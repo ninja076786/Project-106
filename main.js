0bd05715a4af111113e2c6c01215b3c69244e85e
ninja076786 <77653028+ninja076786@users.noreply.github.com>
2021-07-27 15:35:11 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
-if(gh){
+var answer="";
 
+Webcam.set({
+    width:"350",
+    height:"300",
+   image_format:"png",
+   png_quality:120,
+    });
+   
+     var camera = document.getElementById("cam");
+   
+       Webcam.attach("#camera");
+
+function snap(){
+Webcam.snap(function data_url(){
+document.getElementById("outi").innerHTML='<img id="imagecap" src="'+data_url+'">'
+});
 }
